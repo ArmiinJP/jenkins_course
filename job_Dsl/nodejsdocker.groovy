8e8e8f2a89b1037874d4dcbfbefd2bea68651118
armiinjp <arminjp2000@gmail.com>
2022-04-05 13:46:25 +0430

    add nodejsdocker.groovy

---
+++ job_Dsl/nodejsdocker.groovy
@@ -0,0 +1,28 @@
+job('NodeJS_my_Docker') {
+    scm {
+        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+                    
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('arminjp/nodejs-app-pipline-with-jdsl')
+            tag('${GIT_REVISION,length=9}')
+            dockerfileDirectory("./nodejs_app/Dockerfile")
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+            dockerRegistryURL("http://localhost:5000")
+        }
+    }
+}
