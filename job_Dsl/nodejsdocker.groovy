07f986775fd4f7f3671558105df368ee774744b8
Armin Jafarpisheh <58858512+ArmiinJP@users.noreply.github.com>
2022-04-05 14:11:32 +0430

    delete wrapper

--- job_Dsl/nodejsdocker.groovy
+++ job_Dsl/nodejsdocker.groovy
@@ -8,11 +8,7 @@ job('NodeJS_my_Docker') {
     triggers {
         scm('H/5 * * * *')
     }
-    wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
-                    
-    }
+
     steps {
         dockerBuildAndPublish {
             repositoryName('arminjp/nodejs-app-pipline-with-jdsl')
